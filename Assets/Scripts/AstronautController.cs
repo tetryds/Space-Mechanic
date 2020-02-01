8a0284d90d14a79b234832da6d71ab55d4fe3e6d
tetryds <tetryds@hotmail.com>
2020-02-01 07:47:27 -0300

    Did a lot of stuff

---
+++ Assets/Scripts/AstronautController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Astronaut
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class AstronautController : MonoBehaviour
+    {
+        Vector3 translation = Vector3.zero;
+        Vector3 rotation = Vector2.zero;
+
+        Rigidbody rb;
+
+        [SerializeField] float power = 40f;
+        [SerializeField] float torquePower = 10f;
+
+        [SerializeField] float translationDamp = 20f;
+        [SerializeField] float angularDamp = 5f;
+
+        [SerializeField] LayerMask fixPointLayer;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+
+        }
+
+        void Update()
+        {
+            //camAngleAdjust = Vector3.SignedAngle(camera.forward, transform.forward, transform.right);
+            if (Input.GetKeyDown(KeyCode.Joystick1Button5))
+                TryFixNearby();
+        }
+
+        private void FixedUpdate()
+        {
+            translation.x = Input.GetAxisRaw("Horizontal");
+            translation.y = Input.GetAxisRaw("UpDown");
+            translation.z = Input.GetAxisRaw("Vertical");
+            rotation.x = Input.GetAxisRaw("LookUp");
+            rotation.y = Input.GetAxisRaw("LookRight");
+            
+            rb.AddTorque(-rb.angularVelocity * angularDamp * Time.deltaTime);
+            rb.AddRelativeTorque(rotation * torquePower * Time.fixedDeltaTime);
+            rb.AddForce(-rb.velocity * translationDamp * Time.fixedDeltaTime);
+            rb.AddRelativeForce(translation * power * Time.fixedDeltaTime);
+        }
+
+        private void TryFixNearby()
+        {
+            Debug.Log("Trying to fix nearby");
+            Collider[] fixPointCols = Physics.OverlapSphere(transform.position, 2f, fixPointLayer.value);
+
+            foreach (Collider fixPointCol in fixPointCols)
+            {
+                FixPoint fixPoint = fixPointCol.GetComponent<FixPoint>();
+                if (fixPoint != null)
+                {
+                    fixPoint.Fix();
+                    Debug.Log("Fixed nearby");
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/FixPoint.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Astronaut
+{
+    public class FixPoint : MonoBehaviour
+    {
+        [SerializeField] Renderer matRenderer = null;
+        MaterialPropertyBlock materialProperty;
+
+        bool broken = false;
+
+        float timeToBreak;
+        float timeToExplode;
+
+        float maxDuration = 240f;
+        float minDuration = 60f;
+        float maxTimeToExplode = 60f;
+
+        private void Start()
+        {
+            Fix();
+            timeToExplode = maxTimeToExplode;
+            materialProperty = new MaterialPropertyBlock();
+        }
+
+        private void FixedUpdate()
+        {
+
+            if (!broken)
+            {
+                timeToBreak -= Time.fixedDeltaTime;
+                if (timeToBreak <= 0 && !broken)
+                {
+                    Break();
+                }
+                materialProperty.SetColor("_BaseColor", new Color(1 - timeToBreak / maxDuration, timeToBreak / maxDuration, 0));
+                matRenderer.SetPropertyBlock(materialProperty);
+            }
+            else
+            {
+                timeToExplode -= Time.fixedDeltaTime;
+                if (timeToExplode <= 0)
+                {
+                    Explode();
+                }
+            }
+        }
+
+        private void Explode()
+        {
+            Debug.Log("Boom");
+        }
+
+        private void Break()
+        {
+            Debug.Log("Broke");
+            broken = true;
+        }
+
+        public void Fix()
+        {
+            Debug.Log("Fixed");
+            timeToBreak = Random.Range(minDuration, maxDuration);
+            timeToExplode = maxTimeToExplode;
+            broken = false;
+        }
+    }
+}
