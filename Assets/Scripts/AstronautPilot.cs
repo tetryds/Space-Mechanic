e11bc332ff4ceaa7a3964744c02de44dc294449a
tetryds <tetryds@hotmail.com>
2020-02-01 21:37:31 -0300

    New control mode

---
+++ Assets/Scripts/AstronautPilot.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Astronaut
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class AstronautPilot : MonoBehaviour
+    {
+        Vector3 translation = Vector3.zero;
+        Vector3 rotation = Vector2.zero;
+
+        Rigidbody rb;
+
+        [SerializeField] float power = 40f;
+        [SerializeField] float torquePower = 10f;
+
+        [SerializeField] float translationDamp = 20f;
+        [SerializeField] float angularDamp = 5f;
+
+        [SerializeField] float rotDerivativeGain = 0.8f;
+
+        [SerializeField] LayerMask fixPointLayer;
+
+        bool dampen = false;
+
+        AstronautAnimController anim;
+
+        [SerializeField] Toggle isDamping;
+
+        [SerializeField] Transform cameraTransform;
+
+        Vector3 prevRotation = Vector3.zero;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            anim = new AstronautAnimController(GetComponentInChildren<Animator>());
+        }
+
+        void Update()
+        {
+            Debug.DrawRay(transform.position, rb.angularVelocity);
+            //camAngleAdjust = Vector3.SignedAngle(camera.forward, transform.forward, transform.right);
+            if (Input.GetKeyDown(KeyCode.Joystick1Button5))
+                TryFixNearby();
+
+            if (Input.GetKeyDown(KeyCode.Joystick1Button4))
+            {
+                dampen = !dampen;
+                isDamping.isOn = dampen;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            //transform.eulerAngles = cameraTransform.eulerAngles;
+            Vector3 input = Vector3.zero;
+            Vector3 translation = Vector3.zero;
+
+            //Vertical
+            input.y = Input.GetAxisRaw("UpDown");
+            translation.y = input.y;
+            
+            //Horizontal
+            input.x = Input.GetAxisRaw("Horizontal");
+            Vector3 horizontalDir = cameraTransform.TransformDirection(Vector3.right);
+            translation += horizontalDir * input.x;
+
+            //Vertical
+            input.z = Input.GetAxisRaw("Vertical");
+            Vector3 verticalDir = cameraTransform.TransformDirection(Vector3.forward);
+            translation += verticalDir * input.z;
+
+            //Pitch
+            Quaternion pitch = Quaternion.FromToRotation(transform.forward, cameraTransform.forward);
+
+            //Yaw
+            //Roll
+
+            //rb.AddRelativeTorque(pitch.eulerAngles * torquePower * Time.fixedDeltaTime);
+
+            Vector3 headingError = Vector3.Cross(transform.forward, cameraTransform.forward);
+            rb.AddTorque(headingError * torquePower * Time.deltaTime);
+            //transform.LookAt(transform.position + cameraTransform.forward * 10);
+
+            //if (dampen)
+            //    rb.AddForce(-rb.velocity * translationDamp * Time.fixedDeltaTime);
+            translation.Normalize();
+            rb.AddForce(translation * power * Time.fixedDeltaTime);
+
+            Vector3 animDir = transform.InverseTransformDirection(translation);
+            //anim.Forward(translation.z);
+            //anim.Right(-translation.x);
+            anim.Up(animDir.y);
+            //anim.TurnRight(rotation.y);
+
+            //prevRotation = rotation;
+        }
+
+        private void TryFixNearby()
+        {
+            Debug.Log("Trying to fix nearby");
+            Collider[] fixPointCols = Physics.OverlapSphere(transform.position, 2f, fixPointLayer.value);
+
+            foreach (Collider fixPointCol in fixPointCols)
+            {
+                FixPoint fixPoint = fixPointCol.GetComponent<FixPoint>();
+                if (fixPoint != null)
+                {
+                    fixPoint.Fix();
+                    Debug.Log("Fixed nearby");
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Astronaut
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] Transform astronaut = null;
+
+        [SerializeField] float smoothFactor = 10f;
+
+        float mouseSpeedFactor = 0.6f;
+
+        float joystickSpeedFactor = 3f;
+
+        Vector3 mouseStartPos = Vector3.zero;
+
+        Vector3 mouseDelta = Vector3.zero;
+
+        float pitchSpeed = 60f;
+        float yawSpeed = 60f;
+
+        private void Start()
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void Update()
+        {
+            mouseDelta += new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0);
+        //}
+
+        //private void FixedUpdate()
+        //{
+            transform.position += (astronaut.position - transform.position) * smoothFactor * Time.deltaTime;
+
+            float pitch = Input.GetAxisRaw("LookUp") * joystickSpeedFactor + mouseDelta.y * mouseSpeedFactor;
+            float yaw = Input.GetAxisRaw("LookRight") * joystickSpeedFactor + mouseDelta.x * mouseSpeedFactor;
+
+
+            if (pitch < 0 && Vector3.Dot(Vector3.up, transform.forward) < 0.6f ||
+                pitch > 0 && Vector3.Dot(Vector3.up, transform.forward) > -0.6f)
+                transform.Rotate(transform.right, pitch * pitchSpeed * Time.deltaTime, Space.World);
+
+            transform.Rotate(Vector3.up, yaw * yawSpeed * Time.deltaTime, Space.World);
+            mouseDelta = Vector3.zero;
+        }
+    }
+}
