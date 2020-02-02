7e57d4f2e33be3f771760554fc81ac01365847b9
tetryds <tetryds@hotmail.com>
2020-02-02 06:12:14 -0300

    Added visual effects

--- Assets/Scripts/FixPoint.cs
+++ Assets/Scripts/FixPoint.cs
@@ -12,19 +12,29 @@ namespace Astronaut
 
         [SerializeField] TargetIndicator targetIndicator;
 
+        [SerializeField] ParticleSystem smoke;
+        [SerializeField] ParticleSystem explosion;
+
         Renderer matRenderer = null;
         MaterialPropertyBlock materialProperty;
 
         bool broken = false;
+        bool exploded = false;
 
         float timeToBreak;
         float timeToExplode;
 
-        readonly float maxDuration = 120f;
-        readonly float minDuration = 80f;
-        readonly float maxTimeToExplode = 60f;
+        readonly float maxDuration = 30f;
+        readonly float minDuration = 20f;
+        readonly float maxTimeToExplode = 10f;
         readonly float dmgRateWhenBroken = 30f;
         readonly float explosionDmg = 500f;
+
+        //readonly float maxDuration = 120f;
+        //readonly float minDuration = 80f;
+        //readonly float maxTimeToExplode = 60f;
+        //readonly float dmgRateWhenBroken = 30f;
+        //readonly float explosionDmg = 500f;
 
         StationController station;
 
@@ -41,7 +51,7 @@ namespace Astronaut
 
         private void FixedUpdate()
         {
-
+            if (exploded) return;
             if (!broken)
             {
                 timeToBreak -= Time.fixedDeltaTime;
@@ -52,17 +62,19 @@ namespace Astronaut
                 float fixRate = timeToBreak / maxDuration;
                 float breakRate = 1 - fixRate;
                 Color currentState = new Color(breakRate, fixRate, 0);
-                materialProperty.SetColor("_BaseColor", currentState);
-                matRenderer.SetPropertyBlock(materialProperty);
-
                 healthSlider.value = fixRate;
-                sliderImage.color = currentState;
-                targetIndicator.SetColor(currentState);
+                SetColors(currentState);
+                //materialProperty.SetColor("_BaseColor", currentState);
+                //matRenderer.SetPropertyBlock(materialProperty);
+
+                //sliderImage.color = currentState;
+                //targetIndicator.SetColor(currentState);
             }
             else
             {
                 station.Hit(dmgRateWhenBroken * Time.fixedDeltaTime);
                 timeToExplode -= Time.fixedDeltaTime;
+                healthSlider.value = (1 - timeToExplode / maxTimeToExplode);
                 if (timeToExplode <= 0)
                 {
                     Explode();
@@ -72,18 +84,36 @@ namespace Astronaut
 
         private void Explode()
         {
+            smoke.Stop();
             station.Hit(explosionDmg);
+            explosion.Play();
+            exploded = true;
+            SetColors(Color.black);
             Debug.Log("Boom");
         }
 
         private void Break()
         {
+            smoke.Play();
             Debug.Log("Broke");
             broken = true;
         }
 
+        private void SetColors(Color color)
+        {
+            materialProperty.SetColor("_BaseColor", color);
+            matRenderer.SetPropertyBlock(materialProperty);
+
+            
+            sliderImage.color = color;
+            targetIndicator.SetColor(color);
+        }
+
         public void Fix()
         {
+            if (exploded) return;
+
+            smoke.Stop();
             Debug.Log("Fixed");
             timeToBreak = Random.Range(minDuration, maxDuration);
             timeToExplode = maxTimeToExplode;
